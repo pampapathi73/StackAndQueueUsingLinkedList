140720bb32e2f85bd1973cb10c9e25ba9840a530
pampapathi73 <pampapathik32@gmail.com>
2021-06-13 22:02:39 +0530

    [pampapathi] Added: ability to create queue

--- StackAndQueue_UsingLinkedList/Program.cs
+++ StackAndQueue_UsingLinkedList/Program.cs
@@ -9,16 +9,11 @@ namespace StackAndQueue_UsingLinkedList
     {
         static void Main(string[] args)
         {
-            LinkedListStack linkedListStack = new LinkedListStack();
-            linkedListStack.Push(70);
-            linkedListStack.Push(30);
-            linkedListStack.Push(56);
-            linkedListStack.Display();
-            linkedListStack.Peek();
-            linkedListStack.Pop();
-            linkedListStack.Pop();
-            linkedListStack.Display();
-            linkedListStack.Peek();
+            LinkedListQueue linkedListQueue = new LinkedListQueue();
+            linkedListQueue.Enqueue(56);
+            linkedListQueue.Enqueue(30);
+            linkedListQueue.Enqueue(70);
+            linkedListQueue.Display();
         }
 
     }
