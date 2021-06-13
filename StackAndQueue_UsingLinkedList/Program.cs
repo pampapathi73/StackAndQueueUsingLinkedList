841b9a3e505e467e0627ecac49022a72a7314be5
pampapathi73 <pampapathik32@gmail.com>
2021-06-13 21:29:02 +0530

    [pampapathi] Added: implementing stack and queue using linked list

---
+++ StackAndQueue_UsingLinkedList/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackAndQueue_UsingLinkedList
+{
+          
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedListStack linkedListStack = new LinkedListStack();
+            linkedListStack.Push(70);
+            linkedListStack.Push(30);
+            linkedListStack.Push(56);
+            linkedListStack.Display();
+        }
+
+    }
+    }
+
